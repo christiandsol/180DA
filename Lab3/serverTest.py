77bfe98c2a8084b7444e50546278051ccb080874
Christian Solano <christiansolano@Christians-MacBook-Air-3.local>
2025-10-15 16:31:58 -0700

    Fix typos in server lab 3

--- Lab3/serverTest.py
+++ Lab3/serverTest.py
@@ -1,10 +1,10 @@
 import socket
-serv = socket.socket(socket.AF_INET, socket.SOC_STREAM)
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
 while True:
 	conn, addr = serv.accept()
-	from client = ''
+	from_client = ''
 	while True:
 		data = conn.recv(4096)
 		if not data: break
